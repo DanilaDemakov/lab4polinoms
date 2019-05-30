9c6453360328e9b54132a4205a58d16559d3c8d3
DanilaDemakov <kangaroo_00_00@mail.ru>
2019-05-30 15:52:02 +0300

    Added Polinoms func

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include "parserr.h"
+
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+    polinom res1, res2, res3;
+    calculate calculator;
+    while (true) {
+		system("cls");
+        cout << "Введите номер полинома, который хотите поменять. Используйте формат, при вводе полинома:(12x^3y^2z^17+...)\n Для суммы (+) или умножения (*) введите соответствующий знак.\n";
+        cout << " 1) ";
+        res1.h == NULL ? cout << "Пусто" : cout << res1;
+        cout << "\n 2) ";
+        res2.h == NULL ? cout << "Пусто" : cout << res2;
+        cout << "\n 3) Выход.\n";
+        char x;
+        cin >> x;
+            if (x == '1') {
+                string s;
+                cin >> s;
+                res1 = calculator.parser (s);
+            } else
+
+            if (x == '2') {
+                string s;
+                cin >> s;
+                res2 = calculator.parser (s);
+            } else
+                
+            if (x == '+') {
+                if (res1.h == NULL || res2.h == NULL) throw -1;
+                res3 = res1 + res2;
+				if (res3.h == NULL) cout << "0";
+                if (res3.h != NULL) cout << res3;
+                cout << "\n\n Назад в меню (3).\n Если вы хотите, чтобы решение стало 1 или 2 полиномом введите (1) или (2), если нет (3)\n";
+                int g;
+                cin >> g;
+				if (g==1)
+					res1=res3;
+				if (g==2)
+					res2=res3;
+				if ((g<=0)||(g>3)) throw -3;
+            } else
+            
+            if (x == '*') {
+                if (res1.h == NULL || res2.h == NULL) throw -1;
+                res3 = res1 * res2;
+                cout << res3;
+                cout << "\n\n Назад в меню (3).\n Если вы хотите, чтобы решение стало 1 или 2 полиномом введите (1) или (2), если нет (3)\n";
+                int g;
+                cin >> g;
+				if (g==1)
+					res1=res3;
+				if (g==2)
+					res2=res3;
+				if ((g<=0)||(g>3)) throw -3;
+            }
+			else{
+			return 0;
+			}
+    }
+    return 0;    
+}
---
+++ parserr.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "polinom.h"
+#include "monom.h"
+#include <string>
+
+class calculate 
+{
+
+public:
+
+    polinom parser(string s);
+
+};
